56700226a2a3395886c9bac3e06f5893b77dd7fb
pounde candy <poundensigue@gmail.com>
2022-03-26 08:37:30 -0400

    j'ai ajouté l'algorithme pour trouver toute les puissance et un qui retrouve le chiffre exact de la puissance

--- src/com/debut/Leetcode.java
+++ src/com/debut/Leetcode.java
@@ -2,6 +2,33 @@ package com.debut;
 //l'ensemble de mes solution leetcode
 public class Leetcode {
 
+
+    public static boolean ispowerOf(int n, int powerof) {
+        //le meme principe que quand j'essayait de connaitre les puissance étant petit
+        //on divise par le nombre jusqu'a ce qu'on arrive a 1 ou a une division avec reste
+        while (true){
+            if (n==1) return true;//des que n est egal a un on sait que c'est une puissance du nombre
+            if (n==0) return false;// si le nombre est egal a 0 on sait automatiquement que c'est pas une puissance
+            if (n%powerof!=0) return false;//si la division a un reste ce n'est pas une puissance
+            n/=powerof;//on divise le nombre par trois pour pouvoir refaire l'operation
+        }
+    }
+
+    /*l'algorithme sert a connaitre le a quelle puissance est deux nombre
+    exemple: si n est egal a 16 et powerof est egal a 4 la reponse est 2 car 4 puissance 2 egal 16
+    exemple: si n est egal a 128 et powerof est egal a 2 la reponse est 7 car 2 puissance 7 egal 128
+    exemple: si n est egal a 33 et powerof est egal a 2 la reponse est -1 car 33 n'est pas une puissance de 2*/
+    public static int whatisthepower(int n, int powerof) {
+        int compte=0;
+        while (true){
+            if (n==1) return compte;
+            if (n==0) return -1;
+            if (n%powerof!=0) return -1;
+            compte++;
+            n/=powerof;
+        }
+    }
+
     public static boolean isPowerOfThree(int n) {
         //il y'a une boucle infini
         while (true){
@@ -15,7 +42,6 @@ public class Leetcode {
 
 
     }
-    //public static boolean ispowerOf()
 
     public boolean isPowerOfFour(int n) {
         while (true){
--- src/com/debut/Main.java
+++ src/com/debut/Main.java
@@ -1,8 +1,12 @@
 package com.debut;
 
+import static com.debut.Leetcode.whatisthepower;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
     }
 }
