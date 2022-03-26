01f1e97099998097b48757a240884b23eba4dc56
pounde candy <poundensigue@gmail.com>
2022-03-26 08:18:33 -0400

    j'ai ajouté l'algorithme pour les puissances de trois

---
+++ src/com/debut/Leetcode.java
@@ -0,0 +1,24 @@
+package com.debut;
+//l'ensemble de mes solution leetcode
+public class Leetcode {
+
+    public static boolean isPowerOfThree(int n) {
+        //il y'a une boucle infini
+        while (true){
+            //le meme principe que quand j'essayait de connaitre les puissance étant petit
+            //on divise par le nombre jusqu'a ce qu'on arrive a 1 ou a une division avec reste
+
+            if (n==1) return true;//des que n est egal a un on sait que c'est une puissance du nombre
+
+            if (n==0) return false;// si le nombre est egal a 0 on sait automatiquement que c'est pas une puissance
+
+
+            if (n%3!=0) return false;//si la division a un reste ce n'est pas une puissance
+
+            n/=3; //on divise le nombre par trois pour pouvoir refaire l'operation
+        }
+    }
+
+
+
+}
