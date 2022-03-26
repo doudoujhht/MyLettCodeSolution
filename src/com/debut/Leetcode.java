c8c036cc7c1c47c31542a3a869346a47c2896865
pounde candy <poundensigue@gmail.com>
2022-03-26 10:18:15 -0400

    wallah j'ai la flemme so je vag dessus

--- src/com/debut/Leetcode.java
+++ src/com/debut/Leetcode.java
@@ -263,6 +263,21 @@ public class Leetcode {
     }
 
 
+    //ca compte les nombres negatif dans un array
+    public static int countNegatives(int[][] grid) {
+        int compte=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j]<0){
+                    compte += grid[i].length-j;
+                    break;
+                }
+            }
+        }
+        return compte;
+    }
+
+
 
 
 
